9f23e49588e02925c7d361e4aed134c7db045cd3
raygj <jray@JGRMBP15.fios-router.home>
2017-06-02 15:15:30 -0400

    added Systems Manager

--- samples/aws_ec2_install.sh
+++ samples/aws_ec2_install.sh
@@ -6,3 +6,15 @@ sudo apt-get update
 sudo apt-get install -y ansible
 sudo mv /etc/ansible/hosts /etc/ansible/hosts.orig
 sudo nano /etc/ansible/hosts
+sudo apt install python-pip
+pip install --upgrade pip
+pip install awscli
+
+#Systems Manager agent installation
+#http://docs.aws.amazon.com/systems-manager/latest/userguide/ssm-agent.html#sysman-install-startup-linux
+#
+#!/bin/bash
+cd /tmp			
+wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+sudo dpkg -i amazon-ssm-agent.deb
+sudo systemctl enable amazon-ssm-agent
